b3a1c329dfd996614574e8e0a0eea838fbf34632
Jerke Combee <jerke@elgentos.nl>
2022-02-27 11:42:19 +0100

    Implement character list

---
+++ lib/graphql.ts
@@ -0,0 +1,11 @@
+export default async function (query: string) {
+    // @ts-ignore
+    const res = await fetch(process.env.GRAPHQL_URL, {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({query})
+    })
+    return await res.json();
+};
